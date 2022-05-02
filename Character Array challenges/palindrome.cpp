afbd855f31a8dc474544124733a449aeea020574
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-05-02 16:44:19 +0530

    Add files via upload

---
+++ Character Array challenges/palindrome.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+        int n;
+        cin >> n;
+        char arr[n+1];
+        cin>>arr;
+
+        bool check = false;
+        for(int i = 0; i < n; i++)
+        {
+                if(arr[i] != arr[n-i-1])
+                {
+                        check = true;
+                        break;
+                }
+        }
+
+        if(check)
+        {
+                cout << "It is not palindrome.";
+        }
+        else
+                cout << "It is palindrome.";
+        return 0;
+}
