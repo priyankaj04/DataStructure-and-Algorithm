d008c310bf7f00e9b79f27b5551054d0e0e07368
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-07-11 05:48:13 +0530

    Update 2powern.cpp

--- Recursion/2powern.cpp
+++ Recursion/2powern.cpp
@@ -4,17 +4,11 @@ using namespace std;
 int powerof2(int n)
 {
       if ( n == 0 )
-      {
             return 1;
-      }
       else if ( n == 1 )
-      {
             return 2;
-      }
       else
-      {
             return 2 * powerof2(n - 1);
-      }
 }
 
 int main()
