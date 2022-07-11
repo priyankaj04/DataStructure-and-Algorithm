11ac56ddab37cb8e1083a34549368ddc383c62df
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-07-11 05:49:14 +0530

    Update Sumtilln.cpp

--- Recursion/Sumtilln.cpp
+++ Recursion/Sumtilln.cpp
@@ -4,13 +4,9 @@ using namespace std;
 int recursion ( int num )
 {
       if ( num == 1)
-      {
             return 1;
-      }
       else
-      {
             return num + recursion (num - 1);
-      }
 }
 
 int main()
