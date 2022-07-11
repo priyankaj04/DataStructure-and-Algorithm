fe2a84e35aebace00ce9c59779cc07095d4bef56
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-07-11 05:48:47 +0530

    Update Factorial.cpp

--- Recursion/Factorial.cpp
+++ Recursion/Factorial.cpp
@@ -4,17 +4,11 @@ using namespace std;
 int fact( int n )
 {
       if( n == 1 )
-      {
             return 1;
-      }
       else if ( n == 2)
-      {
             return 2;
-      }
       else
-      {
             return n * fact(n - 1);
-      }
 }
 
 int main()
