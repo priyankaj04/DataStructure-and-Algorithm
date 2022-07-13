ba653239113fa1fd8226f6efc373ffc9188bb48f
Priyanka <103273242+priyankaj04@users.noreply.github.com>
2022-07-14 04:46:29 +0530

    Update combination1.cpp

--- Recursion/combination1.cpp
+++ Recursion/combination1.cpp
@@ -2,7 +2,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void findCombination( int ind, int target, vector<int> &arr, vector<vector<int>> &an
+void findCombination( int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds) 
 {
       if(ind == arr.size())
       {
@@ -20,10 +20,11 @@ void findCombination( int ind, int target, vector<int> &arr, vector<vector<int>>
             findCombination(ind, target - arr[ind], arr, ans, ds);
             ds.pop_back();
       }
+      //not pick element
       findCombination(ind + 1, target, arr, ans, ds);
 }
 
-vector<vector<int>> combinationSum(vector<int> &candidates, int target)
+vector<vector<int>> combinationSum(vector<int> &candidates, int target,  vector<int> &ds)
 {
       vector<vector<int>> ans;
       findCombination(0, target, candidates, ans, ds);
@@ -36,17 +37,25 @@ int main()
       int n;
       cout << "Enter total number of array elements = ";
       cin >> n;
-      int arr[n];
+      vector<int> arr;
       cout << "Enter array elements = ";
       for (int i = 0; i < n; i++)
       {
-            cin >> arr[i];
+            int x;
+            cin >> x;
+            arr.push_back(x);
       }
       int sum;
+      vector<int> ds;
       cout << "Enter a sum value = ";
       cin >> sum;
-      vector<int> ds;
-      findCombination(0, ds, 0, arr, n, s
+      vector<vector<int>> ans = combinationSum(arr, sum, ds);
+      for (int i = 0; i < ans.size(); i++) 
+      {
+            for (int j = 0; j < ans[i].size(); j++)
+                  cout << ans[i][j] << " ";
+            cout << endl;
+      }
       return 0;
 }
 // time complexity = O(2^t * k); 
